5ac2d8d315e1b286dc0d030615f5a16dcbd2ac2b
Jordan Steven Parra Torres <jordan.parra@globant.com>
2021-08-15 20:35:16 -0500

    First Commit

---
+++ force-app/main/default/aura/AccountRelated/AccountRelatedController.js
@@ -0,0 +1,25 @@
+({
+    navToRecord : function (component, event, helper) {
+        var navEvt = $A.get("e.force:navigateToSObject");
+        navEvt.setParams({
+            "recordId": component.get("v.account.Id")
+        });
+        navEvt.fire();
+    },
+    handleSuccess : function(component, event, helper) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "Success!",
+            "message": "The Account's info has been updated.",
+            "type": "success"
+        });
+        toastEvent.fire();
+        var recUpdate = $A.get("e.c:recordUpdated");
+        recUpdate.fire();
+        helper.showHide(component);
+    },
+    handleCancel : function(component, event, helper) {
+        helper.showHide(component);
+        event.preventDefault();
+    }
+})
