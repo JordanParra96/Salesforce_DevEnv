4bbc231f93e34154707ef83e000f12dce96b6f86
Jordan Parra <jordan.parra@coastalcloud.us>
2025-06-24 18:20:06 -0500

    Update import statement in test files to use @lwc/engine-dom

--- force-app/main/default/lwc/comboboxAutocomplete/__tests__/comboboxAutocomplete.test.js
+++ force-app/main/default/lwc/comboboxAutocomplete/__tests__/comboboxAutocomplete.test.js
@@ -1,4 +1,4 @@
-import { createElement } from "lwc";
+import { createElement } from "@lwc/engine-dom";
 import ComboboxAutocomplete from "c/comboboxAutocomplete";
 
 describe("c-combobox-autocomplete", () => {
--- force-app/main/default/lwc/datatableWithGraphql/__tests__/datatableWithGraphql.test.js
+++ force-app/main/default/lwc/datatableWithGraphql/__tests__/datatableWithGraphql.test.js
@@ -1,4 +1,4 @@
-import { createElement } from "lwc";
+import { createElement } from "@lwc/engine-dom";
 import DatatableWithGraphql from "c/datatableWithGraphql";
 
 describe("c-datatable-with-graphql", () => {
